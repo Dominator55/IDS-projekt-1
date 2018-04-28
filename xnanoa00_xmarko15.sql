7ef4cc6cf02152e10064f3d83c16b081a877cc66
pmarko <xmarko15@fit.vutbr.cz>
2018-04-28 10:08:14 +0200

    first procedure

--- xnanoa00_xmarko15.sql
+++ xnanoa00_xmarko15.sql
@@ -273,6 +273,44 @@
         FROM dual;
     END;
   /
+
+/* ---------------------------------------
+ Vypcita kolko priemerne zaplatil zakaznik s cislom id predanym v argumente za jednu letenku.
+ Pokial zakazn9k nekupil ziadne letenky vyvola sa vinimka
+ * --------------------------------------- */
+CREATE OR REPLACE PROCEDURE customer_ticket_avg_cost (id_customer_arg NUMBER) AS
+   BEGIN
+    DECLARE CURSOR cursor_cost is
+    SELECT C.id, C.first_name, C.last_name, T.cost
+    FROM  customers C,  reservations R, tickets T
+    WHERE C.id = id_customer_arg AND C.id = R.created_by AND R.id = T.reservation;
+			id_customer customers.id%TYPE;
+			first_name customers.first_name%TYPE;
+			last_name customers.last_name%TYPE;
+			cost tickets.cost%TYPE;
+			total_cost tickets.cost%TYPE;
+			avg_cost tickets.cost%TYPE;
+			num_tickets NUMBER;
+			BEGIN
+				num_tickets := 0;
+				total_cost := 0;
+				OPEN cursor_cost;
+				LOOP
+					FETCH cursor_cost INTO id_customer, first_name, last_name, cost;
+					EXIT WHEN cursor_cost%NOTFOUND;
+					num_tickets:=num_tickets+1;
+					total_cost := total_cost + cost;
+				END LOOP;
+				CLOSE cursor_cost;
+				avg_cost := total_cost / num_tickets;
+				DBMS_OUTPUT.put_line('Klient ' || first_name || ' avg_cost ' || avg_cost);
+				EXCEPTION WHEN ZERO_DIVIDE THEN
+					BEGIN
+					DBMS_OUTPUT.put_line('Klient s ID: ' || id_customer_arg || ' nema v databazi zadne rezervace.');
+				END;
+			END;
+	END;
+/
 /* ---------------------
   INSERT SAMPLE DATA 
 ---------------------- */
